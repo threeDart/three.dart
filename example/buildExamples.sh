43764455968772ba19a86a765b24e3df0cdc68cc
adam <financeCoding@gmail.com>
2012-09-22 19:39:48 -0700

    Updated js build script. ShaderMaterial.dart is now updated to latest compiler implementation

--- example/buildExamples.sh
+++ example/buildExamples.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-MINFROG=/Application/dart/dart-sdk/bin/dart
+MINFROG=/Applications/dart/dart-sdk/bin/dart2js
 for htmlFile in `find . -type f -name "*.html"`
 do
     htmlDirectory=${htmlFile%/*}
     htmlFileName=${htmlFile##*/}
     name=`echo "$htmlFileName" | cut -d'.' -f1;`
-    echo "RUNNING: $MINFROG --compile-only --out=$htmlDirectory/$name.dart.js $htmlDirectory/$name.dart"
-    $MINFROG --compile-only --out=$htmlDirectory/$name.dart.js $htmlDirectory/$name.dart
+    echo "RUNNING: $MINFROG --verbose --out=$htmlDirectory/$name.dart.js $htmlDirectory/$name.dart"
+    $MINFROG --verbose --out=$htmlDirectory/$name.dart.js $htmlDirectory/$name.dart
     #rm -rf out
 done;
