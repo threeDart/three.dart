399280d289e446720d97f2a932f55af2491448f9
Jesse Vogt <jesse.vogt@gmail.com>
2012-11-06 07:15:02 -0600

    Added example for using the FirstPersonControls class

---
+++ example/controls_firstperson/Controls_FirstPerson.dart
@@ -0,0 +1,85 @@
+import 'dart:html';
+import 'dart:math' as Math;
+import 'package:three/three.dart' as THREE;
+import 'package:three/extras/controls/firstpersoncontrols.dart';
+
+/**
+ * Default First Person Control Scheme:
+ * Use mouse to change camera orientation
+ * Left click: move forward, Right click: move backward
+ * Arrows/WASD: Strafe left/right, move forward/backward
+ * 
+ * Starfield code adapted from: http://creativejs.com/tutorials/three-js-part-1-make-a-star-field/
+ */
+
+class Game {
+  Element container;
+  THREE.PerspectiveCamera camera;
+  THREE.Scene scene;
+  THREE.CanvasRenderer renderer;
+  FirstPersonControls controls;
+  
+  List<THREE.Particle> particles;
+  
+  void init() {
+    container = new Element.tag('div');
+    document.body.nodes.add(container);
+    
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 1000;
+    
+    scene = new THREE.Scene();
+        
+    renderer = new THREE.CanvasRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    particles = new List<THREE.Particle>();
+    
+    makeParticles();
+    
+    container.nodes.add(renderer.domElement);
+    
+    controls = new FirstPersonControls(camera, renderer.domElement);
+  }
+   
+  void makeParticles() {
+    var rng = new Math.Random();
+    for (var zpos = -1000; zpos < 1000; zpos += 20) {
+      var material = new THREE.ParticleCanvasMaterial(color: 0xffffff, program: particleRender);
+      var particle = new THREE.Particle(material);
+      
+      
+      particle.position.x = rng.nextDouble() * 1000 - 500;
+      particle.position.y = rng.nextDouble() * 1000 - 500;
+      particle.position.z = zpos;
+      
+      particle.scale.x = particle.scale.y = 10;
+      
+      scene.add(particle);
+      
+      particles.add(particle);
+    }
+  }
+  
+  void particleRender(var context) {
+    context.beginPath();
+    context.arc(0,0,1,0, Math.PI * 2, true);
+    context.fill();
+  }
+  
+  void animate(timestamp) {
+    window.requestAnimationFrame( animate );
+    controls.update(1.0);
+    render();
+  } 
+  
+  void render() {
+    renderer.render(scene, camera);
+  }
+}
+
+void main() { 
+  var game = new Game();
+  game.init();
+  game.animate(null);
+}
