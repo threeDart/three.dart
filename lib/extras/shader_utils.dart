90de4a27633d773aa476a20a186a41e23087015b
Tiago Cardoso <tiago.cardoso@inevo.pt>
2012-11-13 16:59:17 +0000

    added cube shadding

--- lib/extras/shader_utils.dart
+++ lib/extras/shader_utils.dart
@@ -1,7 +1,35 @@
 library ShaderUtils;
 
+import 'package:three/three.dart' as THREE;
+
 //TODO
 var lib = {
-    "normal": ""
+    "normal": "",
+    'cube': new THREE.ShaderMaterial(
+    
+      uniforms: { 
+        "tCube": new THREE.Uniform(type: "t", value: null ),
+        "tFlip": new THREE.Uniform(type: "f", value: -1 )
+        },
+      
+      vertexShader: 
+         '''varying vec3 vViewPosition;
+            void main() {
+              vec4 mPosition = modelMatrix * vec4( position, 1.0 );
+              vViewPosition = cameraPosition - mPosition.xyz;
+              gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+            }''',
+      
+      
+      fragmentShader: 
+         '''uniform samplerCube tCube;
+            uniform float tFlip;
+            varying vec3 vViewPosition;
+            void main() {
+              vec3 wPos = cameraPosition - vViewPosition;
+              gl_FragColor = textureCube( tCube, vec3( tFlip * wPos.x, wPos.yz ) );
+            }'''
+          )
+    
 };
 
