26484929974cfa5fb0837d2bbf46f5a0dcf4b0a0
Nican <nican132@gmail.com>
2013-04-28 21:29:25 -0400

    It is 2*x*x
    According to
    http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/
    , it is 2*x*x...
    Also reseting the other values to match three.js.

--- lib/src/core/Matrix4.dart
+++ lib/src/core/Matrix4.dart
@@ -512,17 +512,28 @@ class Matrix4 {
     yy = y * y2, yz = y * z2, zz = z * z2,
     wx = w * x2, wy = w * y2, wz = w * z2;
 
-    te[0] = 1 - ( yy + zz );
+    te[0] = 1 - 2 * ( y*y + z*z );
     te[4] = xy - wz;
     te[8] = xz + wy;
 
     te[1] = xy + wz;
-    te[5] = 1 - ( xx + zz );
+    te[5] = 1 - 2 * ( x*x + z*z );
     te[9] = yz - wx;
 
     te[2] = xz - wy;
     te[6] = yz + wx;
-    te[10] = 1 - ( xx + yy );
+    te[10] = 1 - 2 * ( x*x + y*y );
+    
+    // last column
+    te[3] = 0;
+    te[7] = 0;
+    te[11] = 0;
+
+    // bottom row
+    te[12] = 0;
+    te[13] = 0;
+    te[14] = 0;
+    te[15] = 1;
 
     return this;
   }
