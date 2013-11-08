894dcdf7a0eed4b3466bbf3d1b5000a9e9bd1d65
Nelson Silva <nelson.silva@inevo.pt>
2013-11-08 12:11:44 +0000

    Update hop_runner

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -8,8 +8,8 @@ void main(List<String> args) {
   // Analyzer
   //
   addTask('analyze_lib', createAnalyzerTask(['lib/three.dart',
-                                                 'lib/extras/controls/firstpersoncontrols.dart',
-                                                 'lib/extras/controls/trackball.dart',
+                                                 'lib/extras/controls/first_person_controls.dart',
+                                                 'lib/extras/controls/trackball_controls.dart',
                                                  'lib/extras/core/curve_utils.dart',
                                                  'lib/extras/core/shape_utils.dart',
                                                  'lib/extras/font_utils.dart',
@@ -19,7 +19,7 @@ void main(List<String> args) {
                                                  'lib/extras/scene_utils.dart',
                                                  'lib/extras/shader_utils.dart',
                                                  'lib/extras/tween.dart',
-                                                 'lib/src/core/ThreeMath.dart']));
+                                                 'lib/src/core/three_math.dart']));
 
   //
   // Doc generation
