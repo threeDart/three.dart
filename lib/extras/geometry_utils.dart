6dfd365635ed4f94a6f9963d860f15c1a1c94242
Nelson Silva <nelson.silva@inevo.pt>
2013-12-18 13:55:50 +0000

    Added GeometryUtils.merge

--- lib/extras/geometry_utils.dart
+++ lib/extras/geometry_utils.dart
@@ -4,6 +4,123 @@ import "package:three/three.dart";
 import 'package:vector_math/vector_math.dart';
 
 // TODO(nelsonsilva) - Add remaining functions
+
+/// Merge two geometries or geometry and geometry from object (using object's transform)
+merge( geometry, mesh_OR_geometry /* mesh | geometry */, [int materialIndexOffset = 0] ) {
+
+  Matrix4 matrix, normalMatrix;
+  var geometry1 = geometry,
+      vertexOffset = geometry1.vertices.length,
+      uvPosition = geometry1.faceVertexUvs[ 0 ].length,
+      geometry2 = (mesh_OR_geometry is Mesh) ? mesh_OR_geometry.geometry : mesh_OR_geometry,
+      vertices1 = geometry1.vertices,
+      vertices2 = geometry2.vertices,
+      faces1 = geometry1.faces,
+      faces2 = geometry2.faces,
+      uvs1 = geometry1.faceVertexUvs[ 0 ],
+      uvs2 = geometry2.faceVertexUvs[ 0 ];
+
+  if ( mesh_OR_geometry is Mesh ) {
+
+    if (mesh_OR_geometry.matrixAutoUpdate)
+      mesh_OR_geometry.updateMatrix();
+
+    matrix = mesh_OR_geometry.matrix;
+
+    // getNormalMatrix => matrix.invert.transpose
+    normalMatrix = new Matrix4.copy(matrix)
+    ..invert()
+    ..transpose();
+
+  }
+
+  // vertices
+
+  vertices2.forEach((vertex) {
+    var vertexCopy = vertex.clone();
+
+    if ( matrix != null ) matrix.transform3(vertexCopy);
+
+    vertices1.add( vertexCopy );
+  });
+
+  // faces
+
+  faces2.forEach((face) {
+
+    var faceCopy, normal, color,
+        faceVertexNormals = face.vertexNormals,
+        faceVertexColors = face.vertexColors;
+
+    if ( face.size == 3 ) {
+
+      faceCopy = new Face3( face.a + vertexOffset, face.b + vertexOffset, face.c + vertexOffset );
+
+    } else if ( face.size == 4 ) {
+
+      faceCopy = new Face4( face.a + vertexOffset, face.b + vertexOffset, face.c + vertexOffset, face.d + vertexOffset );
+
+    }
+
+    faceCopy.normal.setFrom( face.normal );
+
+    if ( normalMatrix != null) {
+
+      normalMatrix.transform3(faceCopy.normal).normalize();
+
+    }
+
+    faceVertexNormals.forEach((faceVertexNormal) {
+
+      normal = faceVertexNormal.clone();
+
+      if ( normalMatrix != null ) {
+
+        normalMatrix.transform3(normal).normalize();
+
+      }
+
+      faceCopy.vertexNormals.add( normal );
+
+    });
+
+    faceCopy.color.copy( face.color );
+
+    faceVertexColors.forEach((color) {
+      faceCopy.vertexColors.add( color.clone() );
+    });
+
+    faceCopy.materialIndex =  materialIndexOffset;
+
+    // TODO - Check if face.materialIndex should be initialized to zero
+    if (face.materialIndex != null) {
+      faceCopy.materialIndex += face.materialIndex;
+    }
+
+    faceCopy.centroid.setFrom( face.centroid );
+
+    if ( matrix != null ) {
+
+      matrix.transform3(faceCopy.centroid);
+
+    }
+
+    faces1.add( faceCopy );
+
+  });
+
+  // uvs
+
+  uvs2.forEach((uv) {
+
+    var uvCopy = uv.map((u) => new UV().copy(u)).toList();
+
+    uvs1.add( uvCopy );
+
+  });
+
+}
+
 clone( Geometry geometry ) {
 
     var cloneGeo = new Geometry();
