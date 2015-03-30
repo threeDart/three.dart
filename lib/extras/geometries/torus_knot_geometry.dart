a40645ee7df64581e3a3c90b4fcd7102fb8b16e7
brason <brage.son@gmail.com>
2015-03-30 17:56:24 +0200

    Update TorusKnotGeometry

--- lib/extras/geometries/torus_knot_geometry.dart
+++ lib/extras/geometries/torus_knot_geometry.dart
@@ -1,97 +1,91 @@
-part of three;
-
-/**
+/*
  * @author oosmoxiecode
  * based on http://code.google.com/p/away3d/source/browse/trunk/fp10/Away3D/src/away3d/primitives/TorusKnot.as?spec=svn2473&r=2473
+ * 
+ * based on r66
  */
 
+part of three;
+
+/** 
+ * Class for generating torus knot geometries, the particular shape 
+ * of which is defined by a pair of coprime integers, p and q. 
+ * If p and q are not coprime, the result will be a torus link.
+ * 
+ *     var geometry = new TorusKnotGeometry(10.0, 3.0, 100, 16);
+ *     var material = new MeshBasicMaterial(color: 0xffff00);
+ *     var torusKnot = new Mesh(geometry, material);
+ *     scene.add(torusKnot);
+ * 
+ */
 class TorusKnotGeometry extends Geometry {
+  List<List> _grid;
+  
+  /// Creates a new torus knot geometry.
+  TorusKnotGeometry([double radius = 100.0,
+                     double tube = 40.0,
+                     int radialSegments = 64,
+                     int tubularSegments = 8,
+                     double p = 2.0,
+                     double q = 3.0,
+                     double heightScale = 1.0]) : super() {
+    _grid = new List(radialSegments);
 
-  double radius, tube, p, q, heightScale;
-  num segmentsR, segmentsT;
-  List grid;
+    for (var i = 0; i < radialSegments; i++) {
+      _grid[i] = new List(tubularSegments);
+      var u = i / radialSegments * 2 * p * Math.PI;
+      var p1 = _getPos(u, q, p, radius, heightScale);
+      var p2 = _getPos(u + 0.01, q, p, radius, heightScale);
+      var tang = p2 - p1;
+      var n = p2 + p1;
 
-  TorusKnotGeometry([this.radius = 200.0, this.tube = 40.0, this.segmentsR = 64, this.segmentsT = 8, this.p = 2.0,
-      this.q = 3.0, this.heightScale = 1.0])
-      : super() {
+      var bitan = tang.cross(n).normalize();
+      n = bitan.cross(tang).normalize();
 
-    grid = new List(segmentsR);
+      for (var j = 0; j < tubularSegments; j++) {
+        var v = j / tubularSegments * 2 * Math.PI;
+        var cx = -tube * Math.cos(v);
+        var cy = tube * Math.sin(v);
 
-    var tang = new Vector3.zero();
-    var n = new Vector3.zero();
-    var bitan = new Vector3.zero();
-
-    for (var i = 0; i < segmentsR; ++i) {
-
-      this.grid[i] = new List(this.segmentsT);
-
-      for (var j = 0; j < this.segmentsT; ++j) {
-
-        var u = i / this.segmentsR * 2 * this.p * Math.PI;
-        var v = j / this.segmentsT * 2 * Math.PI;
-        var p1 = getPos(u, v, this.q, this.p, this.radius, this.heightScale);
-        var p2 = getPos(u + 0.01, v, this.q, this.p, this.radius, this.heightScale);
-        var cx, cy;
-
-        tang = p2 - p1;
-        n = p2 + p1;
-
-        bitan = tang.cross(n);
-        n = bitan.cross(tang);
-        bitan.normalize();
-        n.normalize();
-
-        cx = -this.tube * Math.cos(v); // TODO: Hack: Negating it so it faces outside.
-        cy = this.tube * Math.sin(v);
-
-        p1.x += cx * n.x + cy * bitan.x;
-        p1.y += cx * n.y + cy * bitan.y;
-        p1.z += cx * n.z + cy * bitan.z;
-
-        this.grid[i][j] = _vert(p1.x, p1.y, p1.z);
-
-      }
-
-    }
-
-    for (var i = 0; i < this.segmentsR; ++i) {
-
-      for (var j = 0; j < this.segmentsT; ++j) {
-
-        var ip = (i + 1) % this.segmentsR;
-        var jp = (j + 1) % this.segmentsT;
-
-        var a = this.grid[i][j];
-        var b = this.grid[ip][j];
-        var c = this.grid[ip][jp];
-        var d = this.grid[i][jp];
-
-        var uva = new UV(i / this.segmentsR, j / this.segmentsT);
-        var uvb = new UV((i + 1) / this.segmentsR, j / this.segmentsT);
-        var uvc = new UV((i + 1) / this.segmentsR, (j + 1) / this.segmentsT);
-        var uvd = new UV(i / this.segmentsR, (j + 1) / this.segmentsT);
-
-        this.faces.add(new Face4(a, b, c, d));
-        this.faceVertexUvs[0].add([uva, uvb, uvc, uvd]);
-
+        var pos = new Vector3.zero()
+          ..x = p1.x + cx * n.x + cy * bitan.x
+          ..y = p1.y + cx * n.y + cy * bitan.y
+          ..z = p1.z + cx * n.z + cy * bitan.z;
+        
+        vertices.add(pos);
+        _grid[i][j] = vertices.length - 1;
       }
     }
 
-    this.computeCentroids();
-    this.computeFaceNormals();
-    this.computeVertexNormals();
+    for (var i = 0; i < radialSegments; ++ i) {
+      for (var j = 0; j < tubularSegments; ++ j) {
+        var ip = (i + 1) % radialSegments;
+        var jp = (j + 1) % tubularSegments;
 
+        var a = _grid[i][j];
+        var b = _grid[ip][j];
+        var c = _grid[ip][jp];
+        var d = _grid[i][jp];
+
+        var uva = new Vector2(i / radialSegments, j / tubularSegments);
+        var uvb = new Vector2((i + 1) / radialSegments, j / tubularSegments);
+        var uvc = new Vector2((i + 1) / radialSegments, (j + 1) / tubularSegments);
+        var uvd = new Vector2(i / radialSegments, (j + 1) / tubularSegments);
+
+        faces.add(new Face3(a, b, d));
+        faceVertexUvs[0].add([uva, uvb, uvd]);
+
+        faces.add(new Face3(b, c, d));
+        faceVertexUvs[0].add([uvb.clone(), uvc, uvd.clone()]);
+      }
+    }
+    
+    computeFaceNormals();
+    computeVertexNormals();  
   }
-
-  num _vert(double x, double y, double z) {
-    vertices.add(new Vector3(x, y, z));
-    return vertices.length - 1;
-  }
-
-  Vector3 getPos(double u, double v, double in_q, double in_p, double radius, double heightScale) {
-
+  
+  Vector3 _getPos(double u, double in_q, double in_p, double radius, double heightScale) {
     var cu = Math.cos(u);
-    var cv = Math.cos(v);
     var su = Math.sin(u);
     var quOverP = in_q / in_p * u;
     var cs = Math.cos(quOverP);
@@ -101,6 +95,5 @@ class TorusKnotGeometry extends Geometry {
     var tz = heightScale * radius * Math.sin(quOverP) * 0.5;
 
     return new Vector3(tx, ty, tz);
-
   }
 }
