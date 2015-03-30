fe820e15d17681ede756196cde8eab89ef327d9b
brason <brage.son@gmail.com>
2015-03-30 13:25:41 +0200

    Update CircleGeometry

--- lib/extras/geometries/circle_geometry.dart
+++ lib/extras/geometries/circle_geometry.dart
@@ -1,49 +1,60 @@
+/*
+ * @author hughes
+ * 
+ * based on r70
+ */
+
 part of three;
 
+/** 
+ * CircleGeometry is a simple shape of Euclidean geometry. It is contructed 
+ * from a number of triangular segments that are oriented around a central 
+ * point and extend as far out as a given radius. It is built counter-clockwise 
+ * from a start angle and a given central angle. It can also be used to create 
+ * regular polygons, where the number of segments determines the number of sides.
+ * 
+ *     var material = new THREE.MeshBasicMaterial(color: 0x0000ff);
+ *     
+ *     var radius = 5, segments = 32;
+ *     var circleGeometry = new CircleGeometry(radius, segments);        
+ *     var circle = new Mesh(circleGeometry, material);
+ *     scene.add(circle);
+ */
 class CircleGeometry extends Geometry {
-  num radius, segments, thetaStart, thetaLength;
-
-  CircleGeometry([this.radius = 50, this.segments = 8, this.thetaStart = 0, this.thetaLength = Math.PI * 2]) : super() {
-    segments = Math.max(3, segments);
-
-    var i,
-        uvs = [],
-        center = new Vector3.zero(),
-        centerUV = new UV(0.5, 0.5);
+  String type = 'CircleGeometry';
+  
+  /// Creates a new circle geometry.
+  CircleGeometry([double radius = 50.0, 
+                  int segments, 
+                  double thetaStart = 0.0, 
+                  double thetaLength =  Math.PI * 2]) {
+    segments = segments != null ? Math.max(3, segments) : 8;
+        
+    List<Vector2> uvs = [];
+    var center = new Vector3.zero();
+    var centerUV = new Vector2(0.5, 0.5);
 
     vertices.add(center);
     uvs.add(centerUV);
 
-    for (i = 0; i <= segments; i++) {
-
+    for (var i = 0; i <= segments; i++) {
       var vertex = new Vector3.zero();
-
       vertex.x = radius * Math.cos(thetaStart + i / segments * thetaLength);
       vertex.y = radius * Math.sin(thetaStart + i / segments * thetaLength);
-
+      
       vertices.add(vertex);
-      uvs.add(new UV((vertex.x / radius + 1) / 2, -(vertex.y / radius + 1) / 2 + 1));
-
+      uvs.add(new Vector2((vertex.x / radius + 1) / 2, -(vertex.y / radius + 1) / 2 + 1));
+    }
+    
+    var n = new Vector3(0.0, 0.0, 1.0);
+    
+    for (var i = 1; i <= segments; i++) {
+      faces.add(new Face3(i, i + 1, 0, [n.clone(), n.clone(), n.clone()]));
+      faceVertexUvs[0].add([uvs[i].clone(), uvs[i + 1].clone(), centerUV.clone()]);
     }
 
-    var n = new Vector3(0.0, 0.0, -1.0);
-
-    for (i = 1; i <= segments; i++) {
-
-      var v1 = i;
-      var v2 = i + 1;
-      var v3 = 0;
-
-      faces.add(new Face3(v1, v2, v3, [n, n, n]));
-      faceVertexUvs[0].add([uvs[i], uvs[i + 1], centerUV]);
-
-    }
-
-    computeCentroids();
     computeFaceNormals();
 
     boundingSphere = new BoundingSphere(radius: radius);
-
   }
-
 }
