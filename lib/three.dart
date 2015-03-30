0255e195a6ba66dc8dec52b9ce600f6f41a2985e
brason <brage.son@gmail.com>
2015-03-30 18:12:58 +0200

    Add reference to RingGeometry

--- lib/three.dart
+++ lib/three.dart
@@ -66,6 +66,7 @@ part 'extras/geometries/text_geometry.dart';
 part 'extras/geometries/torus_geometry.dart';
 part 'extras/geometries/torus_knot_geometry.dart';
 part 'extras/geometries/tube_geometry.dart';
+part 'extras/geometries/ring_geometry.dart';
 
 part 'extras/core/curve.dart';
 part 'extras/core/curve_path.dart';
