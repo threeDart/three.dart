0836615c55fa73ed76f493ac564c11ec991e059b
brason <brage.son@gmail.com>
2015-03-30 17:56:13 +0200

    Update TorusGeometry

--- lib/extras/geometries/torus_geometry.dart
+++ lib/extras/geometries/torus_geometry.dart
@@ -1,63 +1,68 @@
+/*
+ * @author oosmoxiecode
+ * @author mrdoob / http://mrdoob.com/
+ * based on http://code.google.com/p/away3d/source/browse/trunk/fp10/Away3DLite/src/away3dlite/primitives/Torus.as?r=2888
+ * 
+ * based on r66
+ */
+
 part of three;
 
+/**
+ * A class for generating torus geometries
+ * 
+ *     var geometry = new TorusGeometry(10.0, 3.0, 16, 100);
+ *     var material = new MeshBasicMaterial(color: 0xffff00);
+ *     var torus = new Mesh(geometry, material);
+ *     scene.add(torus);
+ */
 class TorusGeometry extends Geometry {
-  num radius, tube, segmentsR, segmentsT, arc;
+  /// Creates a new torus geometry.
+  TorusGeometry([double radius = 100.0, 
+                 double tube = 40.0, 
+                 int radialSegments = 8, 
+                 int tubularSegments = 6, 
+                 double arc = Math.PI * 2]) : super() {
 
-  TorusGeometry([this.radius = 100, this.tube = 40, this.segmentsR = 8, this.segmentsT = 6, this.arc = Math.PI *
-      2]) : super() {
+    List<Vector2> uvs = [];
+    List<Vector3> normals = [];
 
-    var center = new Vector3.zero();
-    var uvs = <UV>[];
-    var normals = <Vector3>[];
+    for (var j = 0; j <= radialSegments; j++) {
+      for (var i = 0; i <= tubularSegments; i++) {
+        var u = i / tubularSegments * arc;
+        var v = j / radialSegments * Math.PI * 2;
 
-    for (var j = 0; j <= this.segmentsR; j++) {
+        var center = new Vector3.zero()
+            ..x = radius * Math.cos(u)
+            ..y = radius * Math.sin(u);
+        
+        var vertex = new Vector3.zero()
+            ..x = (radius + tube * Math.cos(v)) * Math.cos(u)
+            ..y = (radius + tube * Math.cos(v)) * Math.sin(u)
+            ..z = tube * Math.sin(v);
 
-      for (var i = 0; i <= this.segmentsT; i++) {
+        vertices.add(vertex);
 
-        var u = i / this.segmentsT * this.arc;
-        var v = j / this.segmentsR * Math.PI * 2;
-
-        center.x = this.radius * Math.cos(u);
-        center.y = this.radius * Math.sin(u);
-
-        var vertex = new Vector3.zero();
-        vertex.x = (this.radius + this.tube * Math.cos(v)) * Math.cos(u);
-        vertex.y = (this.radius + this.tube * Math.cos(v)) * Math.sin(u);
-        vertex.z = this.tube * Math.sin(v);
-
-        this.vertices.add(vertex);
-
-        uvs.add(new UV(i / this.segmentsT, j / this.segmentsR));
-        normals.add(vertex.clone().sub(center).normalize());
-
+        uvs.add(new Vector2(i / tubularSegments, j / radialSegments));
+        normals.add((vertex - center).normalize());
       }
     }
 
+    for (var j = 1; j <= radialSegments; j++) {
+      for (var i = 1; i <= tubularSegments; i++) {
+        var a = (tubularSegments + 1) * j + i - 1;
+        var b = (tubularSegments + 1) * (j - 1) + i - 1;
+        var c = (tubularSegments + 1) * (j - 1) + i;
+        var d = (tubularSegments + 1) * j + i;
 
-    for (var j = 1; j <= this.segmentsR; j++) {
+        faces.add(new Face3(a, b, d, [normals[a].clone(), normals[b].clone(), normals[d].clone()]));
+        faceVertexUvs[0].add([uvs[a].clone(), uvs[b].clone(), uvs[d].clone()]);
 
-      for (var i = 1; i <= this.segmentsT; i++) {
-
-        var a = (this.segmentsT + 1) * j + i - 1;
-        var b = (this.segmentsT + 1) * (j - 1) + i - 1;
-        var c = (this.segmentsT + 1) * (j - 1) + i;
-        var d = (this.segmentsT + 1) * j + i;
-
-        var face = new Face4(a, b, c, d, [normals[a], normals[b], normals[c], normals[d]]);
-        face.normal.add(normals[a]);
-        face.normal.add(normals[b]);
-        face.normal.add(normals[c]);
-        face.normal.add(normals[d]);
-        face.normal.normalize();
-
-        this.faces.add(face);
-
-        this.faceVertexUvs[0].add([uvs[a].clone(), uvs[b].clone(), uvs[c].clone(), uvs[d].clone()]);
+        faces.add(new Face3(b, c, d, [normals[b].clone(), normals[c].clone(), normals[d].clone()]));
+        faceVertexUvs[0].add([uvs[b].clone(), uvs[c].clone(), uvs[d].clone()]);
       }
-
     }
 
-    this.computeCentroids();
-
+    computeFaceNormals();
   }
 }
